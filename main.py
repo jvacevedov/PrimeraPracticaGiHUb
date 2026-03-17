f9a1bb4cf4ca4821bdfb6c8ab5840ca24fe1bf08
jvacevedov <jvacevedov@gmail.com>
2026-03-17 15:04:23 -0500

    modificando desde la rama3 x2

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ print("Hola mundo") #modificada en Jessicagit swit
 print("Esto esta desde mi rama") #modificada Jessica
 print ("Estos desde mi rama x2")
 print("Modificando desde main")
+#holaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+print("comentado desde la rama3")
+print("comentaaaandoooo desde la ramaaaa3")
